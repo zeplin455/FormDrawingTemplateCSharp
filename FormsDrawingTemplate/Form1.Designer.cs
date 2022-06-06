28d912963a5824f7af1da2ceb9b778bd348fbb12
zeplin455 <jdj.hans@gmail.com>
2022-06-06 12:13:48 +0200

    Initial commit

---
+++ FormsDrawingTemplate/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace FormsDrawingTemplate
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FrameDrawTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // FrameDrawTimer
+            // 
+            this.FrameDrawTimer.Enabled = true;
+            this.FrameDrawTimer.Interval = 16;
+            this.FrameDrawTimer.Tick += new System.EventHandler(this.FrameDrawTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 569);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer FrameDrawTimer;
+    }
+}
+
---
+++ FormsDrawingTemplate/Form1.cs
@@ -0,0 +1,57 @@
+﻿using FormsDrawingTemplate.Drawing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsDrawingTemplate
+{
+    public partial class MainForm : Form
+    {
+        BufferedDrawing drawObj;
+        public MainForm()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+
+            drawObj = new BufferedDrawing(this, RenderExample);
+        }
+
+        private int degrees = 0;
+
+        private void RenderExample(Graphics g)
+        {
+            float ellipseSize = 10;
+
+            float FormBounds = Math.Min(this.ClientRectangle.Width, this.ClientRectangle.Height);
+
+            float radius = (FormBounds / 2) - (ellipseSize * 2);
+
+            float locX = radius * (float)Math.Cos(ConvertDegreesToRadians(degrees));
+            float locY = radius * (float)Math.Sin(ConvertDegreesToRadians(degrees));
+
+            locX += this.ClientRectangle.Width / 2;
+            locY += this.ClientRectangle.Height / 2;
+
+            g.FillEllipse(new SolidBrush(Color.Red), locX, locY, ellipseSize, ellipseSize);
+
+            degrees++;
+        }
+
+        private void FrameDrawTimer_Tick(object sender, EventArgs e)
+        {
+            drawObj.Render();
+        }
+
+        public static double ConvertDegreesToRadians(double degrees)
+        {
+            double radians = (Math.PI / 180) * degrees;
+            return radians;
+        }
+    }
+}
